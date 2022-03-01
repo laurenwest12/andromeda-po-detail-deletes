8ff8a588c3243a2a7a40775db10668f65ee77826
Lauren West <laurenwest12@gmail.com>
2022-03-01 10:01:54 -0500

    Now using mssql

--- index.js
+++ index.js
@@ -2,35 +2,39 @@ const express = require('express');
 const app = express();
 
 const { type } = require('./config.js');
-const { andromedaAuthorization } = require('./authorization.js');
-const { getStartTime, submitStartTime } = require('./functions/runTimes.js');
-const { sendErrorReport } = require('./functions/errorReporting.js');
+// const { andromedaAuthorization } = require('./authorization.js');
+// const { getStartTime, submitStartTime } = require('./functions/runTimes.js');
+// const { sendErrorReport } = require('./functions/errorReporting.js');
+
+const { getSQLServerData } = require('./sql')
 
 const server = app.listen(6000, async () => {
 	console.log('App is listening...');
-	let authorizationResult = await andromedaAuthorization();
+	// let authorizationResult = await andromedaAuthorization();
+	// console.log(authorizationResult)
+	await getSQLServerData()
 
-	if (authorizationResult.indexOf('Error') === -1) {
-		console.log('Authorization complete');
-		const { lastRunTime, nextRunTime } = await getStartTime(type);
+	// if (authorizationResult.indexOf('Error') === -1) {
+	// 	console.log('Authorization complete');
+	// 	///const { lastRunTime, nextRunTime } = await getStartTime(type);
 
-		let allErrors = [
-			{
-				err: 'Test',
-				lastId: 1,
-			},
-		];
+	// 	// let allErrors = [
+	// 	// 	{
+	// 	// 		err: 'Test',
+	// 	// 		lastId: 1,
+	// 	// 	},
+	// 	// ];
 
-		allErrors = allErrors.flat();
+	// 	// allErrors = allErrors.flat();
 
-		if (allErrors.length) {
-			await sendErrorReport(allErrors, type);
-		}
+	// 	// if (allErrors.length) {
+	// 	// 	await sendErrorReport(allErrors, type);
+	// 	// }
 
-		await submitStartTime(type, nextRunTime);
-	}
+	// 	//await submitStartTime(type, nextRunTime);
+	// }
 
-	process.kill(process.pid, 'SIGTERM');
+	//process.kill(process.pid, 'SIGTERM');
 });
 
 process.on('SIGTERM', () => {
